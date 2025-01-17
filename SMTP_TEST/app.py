b47f2d6b189a4db3ec8e285d8f1b2b64cc09fb7d
dhkstnrh <gsw8433@naver.com>
2025-01-17 14:29:46 +0900

    TEST

---
+++ SMTP_TEST/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_mail import Mail, Message
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+mail = Mail(app)
+
+@app.route("/")
+def index():
+    msg = Message(
+        # 메일 제목
+        subject="SMTP MAIL TEST",
+        
+        # 발신자
+        sender=app.config['MAIL_USERNAME'],
+        
+        # 수신자
+        recipients=['gsw8433@naver.com']
+    )
+    msg.body = "SMTP MAIL TEST"
+    mail.send(msg)
+    return "Message sent!"
+
+if __name__ == '__main__':
+    app.run()
